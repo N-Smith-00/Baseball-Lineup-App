748e26aa4de01193a960bd7bc1475bd5370f4a98
Nicholas-Smith463 <Nicholas.Smith463@gmail.com>
2024-09-02 06:28:54 -0400

    create shell for app

--- src/app.py
+++ src/app.py
@@ -1,11 +1,25 @@
 from flask import Flask, redirect, render_template
+from main import App
 
-app = Flask(import_name="Lineup App")
+app = Flask(__name__)
+
+@app.before_request
+def load_data():
+    # load data if it exists, else create app class
+    pass
 
 @app.route('/')
 def index():
-    return redirect("/home", code=302)
+    return redirect('/home', code=302)
 
-@app.route("/home")
+@app.route('/home')
 def home():
-    return "this is the home page"
+    return render_template('home.html')
+
+@app.route('/create-team')
+def create_team():
+    return render_template('createTeam.html')
+
+@app.route('/create-player')
+def create_player():
+    return render_template('createPlayer.html')
